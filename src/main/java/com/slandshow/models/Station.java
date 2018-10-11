ddec8511ca6f91a0ddba68357bd6e9b533fa8e34
SlandShow <slandshow12345@mail.ru>
2018-10-11 19:56:07 +0300

    Add new Station model

---
+++ src/main/java/com/slandshow/models/Station.java
@@ -0,0 +1,46 @@
+package com.slandshow.models;
+
+import java.io.Serializable;
+
+public class Station implements Serializable {
+
+    private String name;
+
+    private String newName;
+
+    private Double latitude;
+
+    private Double longitude;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNewName() {
+        return newName;
+    }
+
+    public void setNewName(String newName) {
+        this.newName = newName;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+}
